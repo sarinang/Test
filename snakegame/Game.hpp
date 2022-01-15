e92885b04a5fd8ceaffeda4a9b9974753da2a002
sarinang <sarinanghshi@gmail.com>
2022-01-15 21:03:14 +0330

    add some file

---
+++ snakegame/Game.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <memory>
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "AssetMan.hpp"
+#include "StateMan.hpp"
+
+enum AssetID
+{
+    MAIN_FONT = 0,
+    GRASS,
+    FOOD,
+    WALL,
+    SNAKE,
+    SNAKE2
+};
+
+struct Context
+{
+    std::unique_ptr<Engine::AssetMan> m_assets;
+    std::unique_ptr<Engine::StateMan> m_states;
+    std::unique_ptr<sf::RenderWindow> m_window;
+    bool isMusicOn = true;
+    bool isHard= false;
+    
+    Context()
+    {
+        m_assets = std::make_unique<Engine::AssetMan>();
+        m_states = std::make_unique<Engine::StateMan>();
+        m_window = std::make_unique<sf::RenderWindow>();
+    }
+};
+
+class Game
+{
+private:
+    std::shared_ptr<Context> m_context;
+    sf::Time TIME_PER_FRAME = sf::seconds(1.f/60.f);
+
+public:
+    Game();
+    ~Game();
+
+    void Run();
+};
